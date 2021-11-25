8a98397b320b49bb74af80a25a15f755849398cd
Alvaro Gabriel <agsa0108@gmail.com>
2021-11-25 19:25:54 -0300

    Update TCPServidor.java

--- Socket_TCP/TCP_1/TCPServidor.java
+++ Socket_TCP/TCP_1/TCPServidor.java
@@ -1,7 +1,5 @@
 package envio_de_multiplas_mensagens_com_timeout;
-//O servidor tem um tempo de funcionamento de 
-//10 segundos até que não possa mais receber
-//mensagens. Ele pode receber varias mensagens até o timeout
+
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.BindException;
@@ -15,12 +13,11 @@ public class TCPServidor {
 		
 		try
 		{
-			//Criar um socket do tipo servidor
+			
 			ServerSocket serverSocket = new ServerSocket(port);
 			System.out.println("Aguardando a conexão de um cliente.");
 			//aceitar a conexão que o cliente realiza
 			Socket socket = serverSocket.accept();
-			//Tempo em que o servidor ficará ativo em milissegundos
 			socket.setSoTimeout(10000); 
 			//Permitir que o servidor receba multiplas mensagens
 			while(true)
